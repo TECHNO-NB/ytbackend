c2170c3a22c39a76f05900a0a8e3a1525b7dddab
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-06-08 15:07:20 +0545

    new

--- controllers/subControllers.js
+++ controllers/subControllers.js
@@ -1,25 +1,25 @@
 const asyncHandler = require("../utils/asyncHandler");
-const  Subscription=require("../models/subscriptionsModal")
-const ApiError= require("../utils/apiError");
-const ApiResponse=require("../utils/apiResonse");
+const Subscription = require("../models/subscriptionsModal");
+const ApiError = require("../utils/apiError");
+const ApiResponse = require("../utils/apiResonse");
 
+exports.subscriptionsControler = asyncHandler(async (req, res) => {
+  const { channel } = req.body;
+  const { _id } = req.user;
 
-exports.subscriptionsControler=asyncHandler(async(req,res)=>{
-   const {channel}=req.body;
-   const {_id}=req.user;
-   if(!channel){
-      throw new ApiError(400,"SomeThing Went Wrong");
-   }
-  const subscribed=await Subscription.create({
-      _id,
-      channel
-   })
-   if(!subscribed){
-      throw new ApiError("Error In Subscriptions")
-   }
+  console.log(channel,_id)
+  if (!channel) {
+    throw new ApiError(400, "SomeThing Went Wrong");
+  }
+  const subscribed = await Subscription.create({
+    subscriber:_id,
+    channel,
+  });
+  if (!subscribed) {
+    throw new ApiError("Error In Subscriptions");
+  }
 
-   res
-   .status(200)
-   .json(new ApiResponse(200,subscribed,"Subscribed Successfully"));
-
-})
+  res
+    .status(200)
+    .json(new ApiResponse(200, subscribed, "Subscribed Successfully"));
+});
